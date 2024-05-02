350d539913ac84c04f79ddfaa12e20a39e060dc5
dacie-00 <162839563+dacie-00@users.noreply.github.com>
2024-05-02 22:06:19 +0300

    Add files via upload

---
+++ rock-paper-scissors-lizard-spock.php
@@ -0,0 +1,60 @@
+<?php
+
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+const LIZARD = "lizard";
+const SPOCK = "spock";
+
+$elementMap =
+    [
+        ROCK => ["wins" => [SCISSORS, LIZARD], "loses" => [PAPER, SPOCK]],
+        PAPER => ["wins" => [ROCK, SPOCK], "loses" => [SCISSORS, LIZARD]],
+        SCISSORS => ["wins" => [PAPER, LIZARD], "loses" => [ROCK, SPOCK]],
+        LIZARD => ["wins" => [PAPER, SPOCK], "loses" => [ROCK, SCISSORS]],
+        SPOCK => ["wins" => [ROCK, SCISSORS], "loses" => [PAPER, LIZARD]]
+    ];
+
+$elements = array_keys($elementMap);
+
+function getOpponentInput($elements): string
+{
+    return $elements[array_rand($elements)];
+}
+
+function validateInput($input, $elements): bool
+{
+    if (!in_array($input, $elements)) {
+        echo "Invalid input!";
+        return false;
+    }
+    return true;
+}
+
+function callTurn($userInput, $opponentInput): void
+{
+    echo "You picked $userInput!\n";
+    echo "Opponent picked $opponentInput!\n";
+}
+
+function playTurn($userInput, $opponentInput, $elementMap): void
+{
+    callTurn($userInput, $opponentInput);
+    if (in_array($opponentInput, $elementMap[$userInput]["wins"])) {
+        echo "You win!\n";
+        return;
+    }
+    if (in_array($opponentInput, $elementMap[$userInput]["loses"])) {
+        echo "You lose!\n";
+        return;
+    }
+    echo "tie!\n";
+}
+
+$userInput = strtolower(readline("Pick your hand! (rock, paper, scissors, lizard, spock) - "));
+$opponentInput = getOpponentInput($elements);
+$inputIsValid = validateInput($userInput, $elements);
+
+if ($inputIsValid) {
+    playTurn($userInput, $opponentInput, $elementMap);
+}
